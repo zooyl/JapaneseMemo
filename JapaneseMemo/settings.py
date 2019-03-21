81d722be383129f564f280817dd6d12006c48e54
Mateusz Natoniewski <natoniewski.m@gmail.com>
2019-03-21 13:50:15 +0100

    Debug

--- JapaneseMemo/settings.py
+++ JapaneseMemo/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'testserver', 'japanese-memo.herokuapp.com']
 
