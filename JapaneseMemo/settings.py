bcda831cb6bda363cb6a18d949340f62375b66e0
Mateusz Natoniewski <natoniewski.m@gmail.com>
2019-03-21 13:07:17 +0100

    Settings 1.3

--- JapaneseMemo/settings.py
+++ JapaneseMemo/settings.py
@@ -102,7 +102,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'Hiragana/static')
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
