f2972de1a7c09b6fb72b1519907be7bd0d2da5d3
Mateusz Natoniewski <natoniewski.m@gmail.com>
2019-04-24 15:23:07 +0200

    admin permissions

--- Hiragana/admin.py
+++ Hiragana/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+import Hiragana.models
 
 # Register your models here.
+
+admin.site.register(Hiragana.models.Stats)
+admin.site.register(Hiragana.models.Levels)
+admin.site.register(Hiragana.models.Hiragana)
