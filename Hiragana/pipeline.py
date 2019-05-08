f6c4b41060a8b97c2248f277ad8be133ca4bb4a4
Mateusz Natoniewski <natoniewski.m@gmail.com>
2019-05-08 12:11:34 +0200

    facebook email

--- Hiragana/pipeline.py
+++ Hiragana/pipeline.py
@@ -1,33 +1,8 @@
 from Hiragana.models import Stats
-from django.contrib.auth.models import User
-
-
-# def create_user_stats(backend, user, response, *args, **kwargs):
-#     print('im in user stats')
-#     if backend.name == 'facebook':
-#         print('im in backend')
-#         if user is None:
-#             Stats.objects.create(user=user)
-#             print('created stats')
-# profile = user.get_profile()
-# print(profile)
-# profile = Stats.objects.create(user=profile)
-# print(profile)
-# profile.save()
-
-# def create_user_stats(backend, user, response, *args, **kwargs):
-#     if backend.name == 'facebook':
-#         profile = user.get_profile()
-#         if profile is None:
-#             print('none - first time')
-#             profile = User.objects.get(user_id=user.id)
-#         profile.gender = response.get('gender')
-#         profile.link = response.get('link')
-#         profile.timezone = response.get('timezone')
-#         profile.save()
 
 
 def create_user_stats(*args, **kwargs):
-    ''' check that the user have an existing profile '''
-    print('check if user profile exists')
+    """
+    create stats for user if he already doesn't have one
+    """
     Stats.objects.get_or_create(user=kwargs.get('user'))
