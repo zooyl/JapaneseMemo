a55e570fd8aadeea7d919446bf17e9a397e06453
Mateusz Natoniewski <natoniewski.m@gmail.com>
2019-05-08 09:55:10 +0200

    heroku pipeline

--- Hiragana/pipeline.py
+++ Hiragana/pipeline.py
@@ -5,8 +5,11 @@ def create_user_stats(backend, user, response, *args, **kwargs):
     print('im in user stats')
     if backend.name == 'facebook':
         print('im in backend')
-        profile = user.get_profile()
-        print(profile)
-        profile = Stats.objects.create(user=profile)
-        print(profile)
-        profile.save()
+        if user is None:
+            Stats.objects.create(user=user)
+            print('created stats')
+        # profile = user.get_profile()
+        # print(profile)
+        # profile = Stats.objects.create(user=profile)
+        # print(profile)
+        # profile.save()
