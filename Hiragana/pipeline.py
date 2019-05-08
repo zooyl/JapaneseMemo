ae1fb4b4d05aa50ab4027a1fe473909c65dce0af
Mateusz Natoniewski <natoniewski.m@gmail.com>
2019-05-08 09:42:22 +0200

    changes in pipeline

--- Hiragana/pipeline.py
+++ Hiragana/pipeline.py
@@ -4,8 +4,9 @@ from Hiragana.models import Stats
 def create_user_stats(backend, user, response, *args, **kwargs):
     print('im in user stats')
     if backend.name == 'facebook':
+        print('im in backend')
         profile = user.get_profile()
-        if profile is None:
-            profile = Profile(user_id=user.id)
+        print(profile)
         profile = Stats.objects.create(user=profile)
+        print(profile)
         profile.save()
